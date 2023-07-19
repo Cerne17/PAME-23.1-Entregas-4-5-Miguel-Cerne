5d02f5fa6f4feead6527f5d7cb9fc4b288e3a189
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2023-07-19 16:17:33 -0300

    consertei bug. DB funciona, agora tenho que implementar as relações entre entidades e auth.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { IngredientModule } from './ingredient/ingredient.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, ProductModule, IngredientModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+    ProductModule,
+    IngredientModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
